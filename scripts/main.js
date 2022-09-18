89ca0abc26527befd2e3748960640c2bb89db70c
ArtemidaGre <99325255+ArtemidaGre@users.noreply.github.com>
2022-09-18 21:47:52 +0300

    first buttons update
    i add buttons to projects files

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,21 @@
 var m_b = document.getElementById('112')
 var s_b = document.getElementById('111')
-
-function mafa(){
-    console.log('second button activated')
+const mainf = {
+    developer: 'Aztec_bx',
+    dev_coutry: 'Russia',
+    dev_age: 14,
+    dev_city: 'Yaroslavl',
+    dev_info: function(){
+        console.log(`hi, its developer my name is ${this.developer}, im ${this.dev_age} years old, from ${this.dev_coutry},${this.dev_city}. I hope you like this site :)`)
+    }
 }
 
-function maf(){
-    console.log('first button acrivated')
+const button_f = {
+    first_button: function(){
+        console.log('first button has been activated')
+    },
+    second_button: function(){
+        console.log('second button has been activated')
+    }
+    
 }
